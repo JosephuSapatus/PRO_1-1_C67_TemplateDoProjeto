cf3f6a85f5bb777f2199e9bcd1ff0c2f8603660f
Eduardo <simas.jeduardo@gmail.com>
2023-03-07 17:00:32 -0300

    Projeto

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyBjQyUIl93XVadd61SPYCsPJqpZTmBep0k",
+        authDomain: "aula-batata.firebaseapp.com",
+        databaseURL: "https://aula-batata-default-rtdb.firebaseio.com",
+        projectId: "aula-batata",
+        storageBucket: "aula-batata.appspot.com",
+        messagingSenderId: "892367432617",
+        appId: "1:892367432617:web:1d5cc9693cf98a172ff2b8"
+      };
+    
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
